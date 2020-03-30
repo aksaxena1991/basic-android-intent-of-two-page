4ffbf17559515f2d5036f6e1b0b0226d0dfd4bc6
Anubhav Saxena <tuthubpro@gmail.com>
2020-03-30 16:24:41 +0530

    Device location using GPS and network is working find and it is completed.

--- app/src/main/java/com/example/anubhav/imageviewer/MainActivity.java
+++ app/src/main/java/com/example/anubhav/imageviewer/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.anubhav.imageviewer;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,6 +24,7 @@ import android.Manifest;
 public class MainActivity extends AppCompatActivity {
 
     private Button letsGoBtn;
+    private LocationListener locationListener;
 
 
     @Override
@@ -36,16 +44,44 @@ public class MainActivity extends AppCompatActivity {
         PermissionListener pL = new PermissionListener() {
             @Override
             public void onPermissionGranted() {
-                Toast.makeText(MainActivity.this, "Permission has been granted",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Permission has been granted", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onPermissionDenied(List<String> deniedPermissions) {
-                Toast.makeText(MainActivity.this, "Permission has been denied",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Permission has been denied", Toast.LENGTH_SHORT).show();
             }
         };
         TedPermission.with(MainActivity.this).setPermissionListener(pL).setPermissions(
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION).check();
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        assert locationManager != null;
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, new LocationListener() {
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+
+            @Override
+            public void onLocationChanged(Location location) {
+                if (location != null) {
+                    Log.e("Latitude: ", "" + location.getLatitude());
+                    Log.e("Longitude: ", "" + location.getLongitude());
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extra) {
+            }
+        });
+
     }
 }
